3c9a7f8f03f54ee1b4c2b465027b99200ced45ac
Estefany Castro <estefanycv74@gmail.com>
2023-04-28 11:03:46 -0500

    Laboratorio 06

---
+++ lab06/tienda/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'tienda'
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('producto/<int:producto_id>/', views.producto, name='producto'),
+    path('categoria/<int:categoria_id>/', views.categoria, name='categoria'),
+    path('categoria/<int:categoria_id>/producto/<int:producto_id>/',
+         views.productoCategoria, name='productoCategoria'),
+]
